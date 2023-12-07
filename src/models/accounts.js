cbc49bf46c6d69ed4fe1ad848221bb2972b45a89
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-12-07 23:51:55 +0300

    Created accounts model

--- src/models/accounts.js
+++ src/models/accounts.js
@@ -1,24 +1,59 @@
-const { Sequalize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'postgresql',
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
 const Account = sequelize.define('Account', {
-    accountNumber: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    balance: {
-        type: DataTypes.Decimal,
-        allowNull: false,
+  accountNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+    validate: {
+      len: [15, 15]
     }
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true
+    }
+  },
+  balance: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+      min: 0
+    }
+  },
+  accountType: {
+    type: DataTypes.ENUM('checking', 'savings', 'credit'),
+    allowNull: false
+  },
+  currency: {
+    type: DataTypes.ENUM('USD', 'EUR', 'GBP'),
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'inactive'),
+    allowNull: false
+  }
+}, {
+  // Other model options go here
+  timestamps: true,
+  hooks: {
+    beforeUpdate: (account, options) => {
+      account.lastUpdated = new Date();
+    }
+  }
 });
+
+// If you have a User model
+const User = require('./user');
+User.hasMany(Account, { as: 'Accounts' });
+Account.belongsTo(User);
 
 module.exports = Account;
