1293cb1ee320643b922b5fe88e1afad17240f681
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-12-25 13:38:55 +0300

    Updated server code

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ import deactivateInactiveAccounts from './middlewares/deactivateInactiveAccounts
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
