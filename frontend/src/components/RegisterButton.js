7501646f8c3ef3325d602b5edf99f60893e3f4f9
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-12-28 11:49:55 +0300

    Restyled the register button

--- frontend/src/components/RegisterButton.js
+++ frontend/src/components/RegisterButton.js
@@ -1,42 +1,59 @@
+import React, { useState } from 'react';
 import {
   Button,
+  Dialog,
   makeStyles
 } from '@material-ui/core';
 import DescriptionIcon from '@material-ui/icons/Description';
+import RegisterForm from './RegisterForm';
 
 const useStyles = makeStyles((theme) => ({
   registerButton: {
     backgroundColor: 'transparent',
     color: theme.palette.primary.main,
     border: `2px solid ${theme.palette.primary.main}`,
-    padding: theme.spacing(1.5, 4),
+    padding: theme.spacing(1, 2),
     borderRadius: theme.spacing(1),
-    fontSize: '1.2em',
+    fontSize: '1em',
     marginTop: theme.spacing(6),
-    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)', // add a subtle shadow
-    transition: '0.3s', // add a transition for hover effect
+    boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
+    transition: '0.3s',
     '&:hover': {
       backgroundColor: theme.palette.primary.main,
       color: theme.palette.common.white,
-      transform: 'scale(1.1)' // scale the button on hover
+      transform: 'scale(1.1)'
     }
   }
 }));
 
-function RegisterButton ({ onClick, className }) {
+function RegisterButton ({ className }) {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Button
-      variant='outlined'
-      size='large'
-      startIcon={<DescriptionIcon />}
-      onClick={onClick}
-      aria-label='Register'
-      className={`${classes.registerButton} ${className}`}
-    >
-      Register
-    </Button>
+    <div>
+      <Button
+        variant='outlined'
+        size='large'
+        startIcon={<DescriptionIcon />}
+        onClick={handleClickOpen}
+        aria-label='Register'
+        className={`${classes.registerButton} ${className}`}
+      >
+        Register
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby='form-dialog-title'>
+        <RegisterForm handleClose={handleClose} />
+      </Dialog>
+    </div>
   );
 }
 
