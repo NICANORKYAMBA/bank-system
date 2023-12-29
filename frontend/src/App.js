c42aa94e2d326e9f31cd6ce002c1e236f78bfe0a
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-12-29 16:35:02 +0300

    Added dashboard route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
+// import LoginForm from './components/LoginForm';
 import Dashboard from './pages/userDashboard';
 import AccountDetails from './pages/accountDetails';
 import Transactions from './pages/transactionDetails';
 
-function App() {
+function App () {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/account-details" component={AccountDetails} />
-        <Route path="/transactions" component={Transactions} />
+        <Route path='/' exact component={HomePage} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/account-details' component={AccountDetails} />
+        <Route path='/transactions' component={Transactions} />
         {/* Add more routes as needed */}
       </Switch>
     </Router>
