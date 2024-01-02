6f4af62d8bff11cfed5808830976c09ff3cf3ddb
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2024-01-02 17:31:07 +0300

    Modularised Icons

---
+++ frontend/src/icons/WithdrawIcon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MoneyOffIcon from '@material-ui/icons/MoneyOff';
+
+function WithdrawIcon() {
+  return <MoneyOffIcon color="secondary" />;
+}
+
+export default WithdrawIcon;
