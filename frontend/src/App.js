31fab97b915c1b1955a6de1dfeb5735888fbf987
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2024-01-02 17:34:35 +0300

    Added route to new dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
-// import LoginForm from './components/LoginForm';
-import Dashboard from './pages/userDashboard';
+// import Dashboard from './pages/userDashboard';
+import Dashboard from './pages/Dashboard';
 import AccountDetails from './pages/accountDetails';
 import Transactions from './pages/transactionDetails';
 
