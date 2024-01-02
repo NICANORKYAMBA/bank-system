46a38f8966cfc6eaf74afbd4b33584d4c72f8899
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2024-01-02 17:32:54 +0300

    Modularised fetch actions

---
+++ frontend/src/api/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const fetchAccounts = async (userId) => {
+  const response = await axios.get(`http://localhost:5000/api/accounts/user/${userId}`, {
+    params: {
+      limit: 10,
+      offset: 0,
+      sort: 'createdAt',
+      order: 'DESC'
+    }
+  });
+  return response.data.accounts;
+};
+
+export const fetchTransactions = async (userId) => {
+  const response = await axios.get(`http://localhost:5000/api/transactions/user/${userId}`, {
+    params: {
+      limit: 10,
+      offset: 0,
+      sort: 'createdAt',
+      order: 'DESC'
+    }
+  });
+  return response.data.transactions;
+};
