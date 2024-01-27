452844d1e530e193b4f547428d2b2abeb618f390
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-01-27 19:57:04 +0300

    Removed unnecessary database

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,24 +4,10 @@ dotenv.config();
 
 export default {
   development: {
-    username: process.env.DB_USER_D,
-    password: process.env.DB_PASSWORD_D,
-    database: process.env.DB_NAME_D,
-    host: process.env.DB_HOST_D,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.DB_USER_T,
-    password: process.env.DB_PASSWORD_T,
-    database: process.env.DB_NAME_T,
-    host: process.env.DB_HOST_T,
-    dialect: 'postgres'
-  },
-  production: {
-    username: process.env.DB_USER_P,
-    password: process.env.DB_PASSWORD_P,
-    database: process.env.DB_NAME_P,
-    host: process.env.DB_HOST_P,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres'
   }
 };
