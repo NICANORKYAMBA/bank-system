fa3da73072c6187c7f3f807280eaf04931b9f30f
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-02-07 21:02:31 +0300

    Added redux store persistence on page reload

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,4 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import registerFormReducer from './reducers/RegisterFormReducers';
 import loginFormReducer from './reducers/LoginFormReducers';
 import dashboardReducer from './reducers/DashboardReducers';
@@ -7,16 +11,27 @@ import formReducer from './reducers/QuickReducers';
 import createAccountFormReducer from './reducers/CreateAccountFormReducers';
 import userReducer from './reducers/userReducer';
 
-const store = configureStore({
-  reducer: {
-    registerForm: registerFormReducer,
-    loginForm: loginFormReducer,
-    dashboard: dashboardReducer,
-    transaction: TransactionReducer,
-    quick: formReducer,
-    createAccountForm: createAccountFormReducer,
-    user: userReducer
-  }
+const rootReducer = combineReducers({
+  registerForm: registerFormReducer,
+  loginForm: loginFormReducer,
+  dashboard: dashboardReducer,
+  transaction: TransactionReducer,
+  quick: formReducer,
+  createAccountForm: createAccountFormReducer,
+  user: userReducer
 });
 
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
